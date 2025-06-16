67660d7b6f23ec38629de456d5eedd077099e11f
Neves369 <douglasbrian369@hotmail.com>
2025-06-16 20:02:36 -0300

    refactor: Simplify RecuperarAcesso component by removing unused imports and integrating password reset functionality

--- src/screens/recuperarAcesso/index.tsx
+++ src/screens/recuperarAcesso/index.tsx
@@ -1,10 +1,7 @@
-// import supabase from "../../api/index";
-import { useForm } from "react-hook-form";
 import logo from "../../assets/logocg.webp";
-import AuthContext from "../../context/auth";
 import { useNavigate } from "react-router-dom";
 import background from "../../assets/fundo.webp";
-import React, { memo, useContext, useState } from "react";
+import React, { memo, useEffect, useState } from "react";
 import {
   Box,
   Card,
@@ -15,70 +12,65 @@ import {
   useMediaQuery,
   CircularProgress,
 } from "@mui/material";
-import UsuarioService from "../../services/UsuarioService";
+import supabase from "../../api";
 
 const RecuperarAcesso: React.FC = () => {
   const navigate = useNavigate();
-  const { signIn } = useContext(AuthContext);
-  //   const [message, setMessage] = useState("");
   const isNonMobile = useMediaQuery("(min-width:600px)");
-  const [submitting, setSubmitting] = useState(false);
 
-  //   const [email, setEmail] = useState("");
-  //   const [message, setMessage] = useState("");
-  //   const [newPassword, setNewPassword] = useState("");
-  //   const [confirmPassword, setConfirmPassword] = useState("");
-  //   const [accessToken, setAccessToken] = useState("");
-  //   const [loading, setLoading] = useState(true);
-  //   const [refreshToken, setRefreshToken] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [refreshToken, setRefreshToken] = useState("");
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  useEffect(() => {
+    const hash = window.location.hash;
+    const params = new URLSearchParams(hash.substring(1));
+    const access_token = params.get("access_token") || "";
+    const refresh_token = params.get("refresh_token") || "";
+    setAccessToken(access_token || "");
+    setRefreshToken(refresh_token || "");
 
-  //   useEffect(() => {
-  //     const hash = window.location.hash;
-  //     const params = new URLSearchParams(hash.substring(1));
-  //     const access_token = params.get("access_token") || "";
-  //     const refresh_token = params.get("refresh_token") || "";
-  //     setAccessToken(access_token || "");
-  //     setRefreshToken(refresh_token || "");
+    session(access_token, refresh_token);
+  }, []);
 
-  //     session(access_token, refresh_token);
-  //   }, []);
+  const session = async (access_token: string, refresh_token: string) => {
+    const { error, data } = await supabase.auth.setSession({
+      access_token: access_token,
+      refresh_token: refresh_token,
+    });
+    if (error) {
+      setMessage(`Erro ao definir sessão: ${error.message}`);
+    } else {
+      setEmail(data.user?.email || "");
+      setLoading(false);
+    }
+  };
 
-  //   const session = async (access_token: string, refresh_token: string) => {
-  //     const { error, data } = await supabase.auth.setSession({
-  //       access_token: access_token,
-  //       refresh_token: refresh_token,
-  //     });
-  //     if (error) {
-  //       setMessage(`Erro ao definir sessão: ${error.message}`);
-  //     } else {
-  //       setEmail(data.user?.email || "");
-  //       setLoading(false);
-  //     }
-  //   };
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    if (newPassword !== confirmPassword) {
+      setMessage("As senhas não coincidem.");
+      return;
+    }
+    if (!accessToken || !refreshToken) {
+      setMessage("Token de acesso inválido.");
+      return;
+    }
 
-  const onSubmit = async (data: any) => {
-    setSubmitting(true);
-    UsuarioService.login(data)
-      .then((resp) => {
-        if (resp.token) {
-          signIn(resp);
-          navigate("/dashboard");
-        } else {
-          window.alert("Não foi possível realizar o login!");
-        }
-      })
-      .catch((e) => {
-        console.log(e);
-      })
-      .finally(() => {
-        setSubmitting(false);
-      });
+    const { error } = await supabase.auth.updateUser({ password: newPassword });
+
+    if (error) {
+      setMessage(`Erro: ${error.message}`);
+    } else {
+      setMessage("Senha redefinida com sucesso!");
+      setTimeout(() => {
+        window.location.reload();
+      }, 4000);
+    }
   };
 
   return (
@@ -94,7 +86,7 @@ const RecuperarAcesso: React.FC = () => {
         backgroundImage: `url(${background})`,
       }}
     >
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit}>
         <Card
           sx={{
             maxWidth: isNonMobile ? 390 : 350,
@@ -112,17 +104,16 @@ const RecuperarAcesso: React.FC = () => {
             Bem-vindo!
           </Typography>
 
+          {message && <p>{message}</p>}
+
           <TextField
             id="Email-basic"
             type="email"
             label="E-mail"
             variant="standard"
             style={{ width: "100%" }}
-            error={!!errors.email}
-            helperText={errors.email ? "Email é obrigatório" : ""}
-            {...register("email", {
-              required: true,
-            })}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
 
           <TextField
@@ -131,11 +122,8 @@ const RecuperarAcesso: React.FC = () => {
             label="Nova Senha"
             variant="standard"
             style={{ width: "100%", marginTop: 20 }}
-            error={!!errors["nova-senha"]}
-            helperText={errors["nova-senha"] ? "Nova Senha é obrigatória" : ""}
-            {...register("nova-senha", {
-              required: true,
-            })}
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
           />
 
           <TextField
@@ -144,22 +132,17 @@ const RecuperarAcesso: React.FC = () => {
             label="Confirmar Senha"
             variant="standard"
             style={{ width: "100%", marginTop: 20 }}
-            error={!!errors["confirmar-senha"]}
-            helperText={
-              errors["confirmar-senha"] ? "Confirmar Senha é obrigatória" : ""
-            }
-            {...register("confirmar-senha", {
-              required: true,
-            })}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
           />
 
           <Button
+            disabled={loading}
             type="submit"
-            disabled={submitting}
             style={{ marginTop: 50, marginBottom: 5, width: "100%" }}
             variant="contained"
           >
-            {submitting ? (
+            {loading ? (
               <CircularProgress size={24} sx={{ color: "white" }} />
             ) : (
               "Login"
